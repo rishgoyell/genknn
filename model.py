ee2710072d76452b8fbb8f4f4a2c7a40d8f23837
Rishabh Goyal <risgoyal@iitk.ac.in>
2017-11-01 22:06:07 +0530

    collapsed version of model, only prototypes learnt

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import six
 import tensorflow as tf
-
+import pickle as pk
 
 from edward.models import (
     Categorical, Dirichlet, Empirical, InverseGamma,
@@ -45,27 +45,43 @@ np.place(Xscale, Xscale==0, -1)
 Xtrain = (Xtrain-Xmean)/Xscale
 np.place(Xscale, Xscale==-1, 0)
 
+inference = 'EM'
+model = 'collapsed'
 
 
 # model
-print("Defining model...........")                                                                                                                                          
-beta = Dirichlet(tf.ones(M))                                                                                                                     
-mu = Normal(tf.zeros(D1), tf.ones(D1), sample_shape=M)                                                                                           
-sigmasq = InverseGamma(tf.ones(D1), tf.ones(D1), sample_shape=M)                                                                                 
-z = ParamMixture(beta, {'loc': mu, 'scale_diag': tf.sqrt(sigmasq)},                                                                              
-              MultivariateNormalDiag,                                                                                                         
-              sample_shape=N)                                                                                                                 
-c = z.cat                                                                                                                                        
-wx = Normal(loc=tf.zeros([D2, D1]), scale=tf.ones([D2, D1]))                                                                                     
-wy = Normal(loc=tf.zeros([10, D1]), scale=tf.ones([10, D1]))                                                                                     
-x = Normal(loc=tf.matmul(z, wx, transpose_b=True), scale=tf.ones([N, D2]))                                                                                         
-y = Categorical(logits=tf.matmul(z, wy, transpose_b=True))                                                                                                         
+print("Defining model...........")
+if model != 'collapsed':                                                                                                                                          
+    beta = Dirichlet(tf.ones(M))                                                                                                                     
+    mu = Normal(tf.zeros(D1), tf.ones(D1), sample_shape=M)                                                                                           
+    sigmasq = InverseGamma(tf.ones(D1), tf.ones(D1), sample_shape=M)                                                                                 
+    z = ParamMixture(beta, {'loc': mu, 'scale_diag': tf.sqrt(sigmasq)},                                                                              
+                  MultivariateNormalDiag,                                                                                                         
+                  sample_shape=N)                                                                                                                 
+    c = z.cat                                                                                                                                        
+    wx = Normal(loc=tf.zeros([D2, D1]), scale=tf.ones([D2, D1]))                                                                                     
+    wy = Normal(loc=tf.zeros([10, D1]), scale=tf.ones([10, D1]))                                                                                     
+    x = Normal(loc=tf.matmul(z, wx, transpose_b=True), scale=tf.ones([N, D2]))                                                                                         
+    y = Categorical(logits=tf.matmul(z, wy, transpose_b=True))
+
+else:
+    beta = Dirichlet(tf.ones(M))
+    mu = Normal(tf.zeros(D1), tf.ones(D1), sample_shape=M)
+    sigmasq = InverseGamma(tf.ones(D1), tf.ones(D1), sample_shape=M)
+    cat = Categorical(probs=beta, sample_shape=N)
+    components = [
+    MultivariateNormalDiag(mu[k], sigmasq[k], sample_shape=N)
+    for k in range(M)]
+    z = Mixture(cat=cat, components=components,sample_shape=N)
+    wx = Normal(loc=tf.zeros([D2, D1]), scale=tf.ones([D2, D1]))
+    wy = Normal(loc=tf.zeros([1, D1]), scale=tf.ones([1, D1]))
+    x = Normal(loc=tf.matmul(z, wx, transpose_b=True), scale=tf.ones([N, D2]))
+    y = Bernoulli(logits=tf.matmul(z, wy, transpose_b=True))
+
 
       
 
 # inference
-inference = 'EM'
-
 if inference == 'VI':
     qz = Normal(loc=tf.Variable(tf.random_normal([N, D1])),
                 scale=tf.nn.softplus(tf.Variable(tf.random_normal([N, D1]))))
@@ -104,8 +120,8 @@ if inference == 'EM':
     qwy = PointMass(params=tf.Variable(tf.random_normal([10, D1])))
     qsigmasq = PointMass(params=tf.Variable(tf.ones([M,D1])))
     
-    inference_e = ed.KLqp({z:qz,c:qc}, data={x: data.X, y: data.Y, mu:qmu, wx:qwx, wy:qwy, sigmasq:qsigmasq})
-    inference_m = ed.MAP({mu:qmu, wx:qwx, wy:qwy, sigmasq:qsigmasq}, data={x: Xtrain, y: Ytrain})
+    inference_e = ed.KLqp({z:qz}, data={x: data.X, y: data.Y, mu:qmu, wx:qwx, wy:qwy, sigmasq:qsigmasq})
+    inference_m = ed.MAP({mu:qmu, wx:qwx, wy:qwy, sigmasq:qsigmasq}, data={x: Xtrain, y: Ytrain, z:qz})
 
     inference_e.initialize(optimizer = tf.train.AdamOptimizer(learning_rate=1e-3))
     inference_m.initialize()
@@ -124,28 +140,33 @@ if inference == 'EM':
 sess = ed.get_session()
 
 if inference in ['VI', 'EM']:
-    probs = sess.run(qc.probs)
-    cluster = np.argmax(probs, axis=1)
-    clusterlabels = np.zeros([M, 10])
-    for i in range(M):
-        temp = Ytrain[np.where(cluster==i)]
-        elem, count = np.unique(temp, return_counts=True)
-        elem = elem.astype(int)
-        for j in range(elem.shape[0]):
-            clusterlabels[i,elem[j]] = count[j]
+    # probs = sess.run(qc.probs)
+    # cluster = np.argmax(probs, axis=1)
+    # clusterlabels = np.zeros([M, 10])
+    # for i in range(M):
+    #     temp = Ytrain[np.where(cluster==i)]
+    #     elem, count = np.unique(temp, return_counts=True)
+    #     elem = elem.astype(int)
+    #     for j in range(elem.shape[0]):
+    #         clusterlabels[i,elem[j]] = count[j]
         
 zproto = sess.run(qmu.params)
 weightx = sess.run(qwx.params)
 weighty = sess.run(qwy.params)
 xcenters = np.matmul(zproto,weightx.transpose())*Xscale+Xmean
 ycenters = np.matmul(zproto, weighty.transpose())
+xlist = []
+ylist = []
 
 for i in range(xcenters.shape[0]):
-    if inference in ['VI', 'EM']:
-        print(clusterlabels[i,:].astype(int))
-    print(ycenters[i,:])
+    # if inference in ['VI', 'EM']:
+    #     print(clusterlabels[i,:].astype(int))
+    ylist.append(ycenters[i,:])
+    xlist.append(xcenters[i,:].reshape((28,28)))
     # utils.save(xcenters[i,:].reshape((28,28)), 'MNIST/xprotoMAP/'+str(i)+'.png')
 
+with open('collapsed'+'_'+str(M)+'_'+str(D1)+'.npz','wb') as outfile:
+    pk.dump([xlist, ylist], outfile)
 
 # dictionary = np.matmul(zproto,dictionary.transpose())*Xscale+Xmean
 # np.place(dictionary, dictionary<0, 0)
